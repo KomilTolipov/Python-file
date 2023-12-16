d5aa751ac3431475e4a9b934ae5275082b601396
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:08:45 +0500

    Add files via upload

---
+++ data.py
@@ -0,0 +1,23 @@
+# 1-misol----------------------------
+import os
+
+# file=os.listdir('data')
+# print(file)
+
+# 2-misol-----------------------------
+# import os
+
+# os.chdir('data2')
+# file=os.getcwd()
+# print(file)
+
+# 3-misol------------------------------
+
+# a=input('fayl/papka: ')
+
+# if a=='papka':
+#     os.mkdir('new_folder')
+
+# else:
+#     with open('new_file.py','w') as file:
+#         file.write('komil')
