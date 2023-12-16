c55cdc88d5895f357ab712732fcfc3b1c7492ab1
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:02:34 +0500

    Add files via upload

---
+++ dars10.py
@@ -0,0 +1,35 @@
+# elementlarga murojaat
+
+# a=(1,2,3,4)
+# print(a[3])
+
+#Indeks oralig’i
+
+# a=tuple(range(10,101,2))
+# print(a[2:10:2])
+
+#Elemnt qiymatlarini o’zgartirish
+
+# a=tuple(range(10,101,2))
+# b=list(a)
+# b.append(3000)
+# a=tuple(b)
+# print(a)
+
+# count
+
+# a=(1,2,3,4,5,6,7,8,9,9,9)
+# b=a.count(9)
+# print(b)
+
+# index
+# a=(1,2,3,4,6,7,7,8,9)
+# b=a.index(7,5,9)
+# print(b)
+
+# a=tuple(range(10,1000,100))
+# b=list(a)
+# b.insert(3000,2)
+# a=tuple(b)
+# print(a)
+
