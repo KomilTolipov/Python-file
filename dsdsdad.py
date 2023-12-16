92d003001b55cb444e60e98ed9edf9f931bfc8f2
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:11:45 +0500

    Add files via upload

---
+++ dsdsdad.py
@@ -0,0 +1,65 @@
+#1-msiol
+
+
+# toq_son = []
+# juft_son = []
+
+# son = 1
+# while son <= 20:
+#     if son % 2 == 0:
+#         juft_son.append(son)
+#     else:
+#         toq_son.append(son)
+#     son +=1
+# print("Toq sonlar:", toq_son)
+# print("Juft sonlar:", juft_son)
+
+
+#2-misol
+
+# son = 1
+
+# while son <=5:
+#     takrorlash_soni=1
+#     while takrorlash_soni <=4:
+#         takrorlash_soni+=1
+#         print(son)
+#     son+=1
+
+#4-mashq
+# family = ["muxtabar", "davron", "munisa", 'komil'] 
+
+# index=0
+# while index < len(family):
+#     print(family[index])
+#     index+=1
+
+#5-mashq
+
+# toq_list = []
+# juft_list = []
+
+# son = int(input("Istalgan son kiriting: "))
+
+# while son!=0:
+#     if son %2==0:
+#         juft_list.append(son)
+#     else:
+#         toq_list.append(son)
+# print("Toq sonlar:", toq_list)
+# print("Juft sonlar:", juft_list)
+
+# for i in ['Dadam','Oyim','Opam','man','singlim']:
+#     print(i)
+
+# son=range(1,10)
+# for i in son:
+#     if i%2==1:
+#         print(i)
+
+
+# a=[]
+# for i in ['Dadam','Oyim','Opam','man','singlim']:
+#     if 'man'in i:
+#         a.append(i)
+#         print(a)
