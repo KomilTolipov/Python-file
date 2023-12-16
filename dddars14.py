c5a4085e9b5e9b1c28cdda85125ef5b61487cfe0
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:09:30 +0500

    Add files via upload

---
+++ dddars14.py
@@ -0,0 +1,70 @@
+#1-misol
+
+# a=0
+# while a<10:
+#     a=a+1
+#     print(a)
+
+#2-misol
+
+# a=25
+# while 20<a:
+#     a=a+a
+#     print(a)
+
+#3-misol
+# a=0
+# while a<15:
+#     a=a+1
+#     print(a)
+
+#4-misol
+
+# a=0
+# while a<15:
+#     a=a+1
+#     print(a)
+
+#5-misol
+
+# yil=2009
+
+# while yil<2023:
+#     yil=yil+1
+#     print(yil)
+
+#6-misol
+# a=0
+# while a<20:
+#     a=a+1
+#     if 15==15:
+#         continue
+#     print(a)
+
+#7-misol
+
+# a=0
+# while a<9:
+#     a=a+1
+#     if 0==0:
+#      continue 
+#     print(a)
+
+#8-mashq
+
+# a=0
+# while a<10:
+#     a=a+2
+#     print(a)
+#     if 10==10:
+#         a=a+20
+#         print(a)
+
+#9-mashq
+
+a=0
+while a<3:
+    a=a+1
+    
+        a=a+30
+    
