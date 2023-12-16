1ed079c98219ed825518502d9091c0f8a792e069
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:09:09 +0500

    Add files via upload

---
+++ ddars14.py
@@ -0,0 +1,37 @@
+#1-misol
+# a=int(input('Ismingizni kirgizing-'))
+
+# if a=='Admin':
+#     print('Xush kelibsiz admin foydalanuvchilar royxatini korasizmi?')
+# else:
+#     print('notogri parol')
+
+#2-misol
+# son=int(input('Son kirgizing '))
+# son1=int(input('Son kirgizing '))
+
+# if son==son1:
+#     print('Sonlar teng ekan')
+
+#3-misol
+
+# a=int(input('Son kirgizing- '))
+# if a>0:
+#     a=a-4
+#     print(a)
+# else:
+#     print('Musbat son kirgizing')
+
+# a=int(input('Son kirgizing '))
+# b=int(input('Son kirgizing '))
+# if a==b:
+#     print('Sonlar teng ekan')
+# else:
+#     print('sonlar teng emas')
+
+d=int(input('Son kirgizing 2'))
+if d>0:
+    d=d**4
+    print(d)
+else:
+    print('Musbat son kirgizing')
