e1f92ba55d2d733f2af5fc4197b1786ba8abb1e5
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:11:24 +0500

    Add files via upload

---
+++ dsads.py
@@ -0,0 +1,24 @@
+# a=int(input("a="))
+# b=int(input("b="))
+# S=a*b
+# print(S)
+
+# d=int(input('d='))
+# p=3.14
+# L=p**d
+# print(L)
+
+# a=int(input('s='))
+# b=a*a*a
+# print(a)
+
+# a=int(input('a='))
+# b=int(input('b='))
+# l=(a+b)/2
+# print(l)
+
+# a=int(input('a='))
+# b=int(input('b='))
+# l=(a*b)**(1/2)
+# print(l)
+
