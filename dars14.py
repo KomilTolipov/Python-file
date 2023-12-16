208b713ee6bfdb5c9fe546ace506381e706d0b87
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:08:29 +0500

    Add files via upload

---
+++ dars14.py
@@ -0,0 +1,93 @@
+#1-mashq
+#-------------------------------
+# a=int(input('a='))
+# b=int(input('b='))
+
+
+# if a<b:
+#     print('a')
+# elif a>b:
+#     a=a+b
+#     print(a)
+
+
+#2-mashq
+#-------------------------------
+# a=int(input('a='))
+# b=int(input('b='))
+# c=int(input('c='))
+
+# if a>c and a>b:
+#     print(a)
+# elif b>c and b>a:
+#     print(b)
+# elif c>b and c>a:
+#     print(c)
+# else:
+#     print('son kriting')
+
+
+#3-mashq
+#--------------------------------
+# a=int(input('a='))
+# b=int(input('b='))
+# c=int(input('c='))
+
+# if a<c and a<b:
+#     print(a)
+# elif b<c and b<a:
+#     print(b)
+# elif c<b and c<a:
+#     print(c)
+# else:
+#     print('son kriting')
+
+#4-mashq
+# a=int(input('a='))
+# b=int(input('b='))
+# c=int(input('c='))
+
+# if a<c and a>b:
+#     print(a)
+# elif b<c and b>a:
+#     print(b)
+# elif c<b and c>a:
+#     print(c)
+# else:
+#     print('son kriting')
+
+#6-mashq
+#------------------------------
+# mashina=input('mashina=')
+# mashina1=input('mashina=')
+
+# if mashina1 not in mashina:
+#     mashina=mashina+mashina1
+#     print(mashina)
+# elif mashina1 in mashina:
+#     print('sizda mashina mavjud')
+# else:
+#     print('mashina rusumini kririting')
+
+#7=mashq
+#---------------------------------------------
+# a=int(input('a='))
+# b=int(input('b='))
+
+# print(a) if a<b and print(b) else a>b
+
+
+#8-mashq
+#--------------------------------------------
+
+# a=int(input('a='))
+# b=int(input('b='))
+# if a==b:
+#     a=a*b
+#     print(a)
+# else:
+#     a=a/b
+#     print(a)
+
+
+
