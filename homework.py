272288c656eb0848980603d162987b4607840faf
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:15:50 +0500

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,64 @@
+# a="Oilamiz azolari_"
+# b="Davron"
+# c="Muxtabar"
+# d="Muxlisa"
+# e="Komil"
+# f="Munisa"
+
+# print(a,b,c,d,e,f)
+
+# ism="Dostlarim"
+# a="Nurmuhammad"
+# b="Abdulloh"
+# c="Said"
+
+# print(ism,a,b,c)
+
+# a=40
+# b=20
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a**b)
+# print(a%b)
+
+# x=30
+# y=50
+
+# x=y
+# print(x,y)
+
+# a=45
+# b=2
+# print(a**b)
+
+# a=45
+# b=3
+# print(a**b)
+
+# a=37
+# b=2023
+# print(b-a)
+
+# a=25
+# b=12
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# a=20
+# b=30
+# c=30
+# d=50
+ 
+# d=c
+# b=d
+# a=c
+# c=a
+# d=a
+# print(a,b,c,d)
+x
+xhjhhhkkhk
