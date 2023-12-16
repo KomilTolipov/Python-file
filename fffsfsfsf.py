d950db41bf29c66429df634257146a19c31048b5
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:14:23 +0500

    Add files via upload

---
+++ fffsfsfsf.py
@@ -0,0 +1,79 @@
+# 1
+
+# toq_son=[]
+# juft_son=[]
+
+# f=0
+# for i in range(1,21):
+#     if f %2==0:
+#         juft_son.append(f)
+#     else:
+#         toq_son.append(f)
+#     f +=1    
+#     print("Toq sonlar:", toq_son)
+#     print("Juft sonlar:", juft_son)
+
+# 2
+
+
+# for i in range(1,6):
+#     a=0
+#     while a<=4:
+#         a=a+1
+#         print(i)
+
+
+# 3
+
+# a='komil'
+
+# for i in a:
+#     if i=='o':
+#         print(i)
+#     elif i=='k':
+#         print(i)    
+
+# 4
+
+# oila=['davron','muhtabar','muxlisa','komil','munisa']
+
+# for i in oila:
+#     print(i)
+
+#13-misol
+# son = int(input("iltimos, son kiriting: "))
+
+# while son !=0:
+#     print(f"siz kiritgan son: {son}")
+#     son = int(input("keyingi sondan kiritish uchun qaytadan son kiriting: "))
+
+# print("nol kiritmang sikl tugadi.")
+
+# def full_name():
+#     print('Tolipov Komil')
+
+# print(full_name())
+
+# def son():
+#     a=int(input('Son kiriting: '))
+#     b=int(input('Son kiriting: '))
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+#     print(a/b)
+
+# print(son())
+
+# a=[0,2,4,6,8,10]
+# def make():
+#     for i in a:
+#      print(i)
+
+# print(make())
+
+a=range(0,100,3)
+def son():
+    for i in a :
+        print(i)
+
+print(son())
