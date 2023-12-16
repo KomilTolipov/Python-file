6bd36faddc6523371d219ed18cfced57ec3e5ff9
komil tolipov <151649825+KomilTolipov@users.noreply.github.com>
2023-12-16 16:08:14 +0500

    Add files via upload

---
+++ dars13.py
@@ -0,0 +1,17 @@
+# a=[1,2,3,42,3,43,4,3]
+# b=a.pop(4)
+# print(b)
+
+# a=(1,2,12,1,2,12)
+# b=a.index(12)
+# print(b)
+
+# a={12,31,31,23,123,1,23,1}
+# b={1,321,23,12,3,12}
+# d=a.update(b)
+# print(a)
+
+import random 
+
+a=('support us ')
+
